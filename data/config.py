2577e6c81e771b0949cf71f45bc19a7fea69f4ee
Leonid Krapchatov <hellsing1998nsk@gmail.com>
2021-09-21 01:40:04 +0300

    Add a handler that records new users in the database and gives the main menu

--- data/config.py
+++ data/config.py
@@ -4,6 +4,7 @@ env = Env()
 env.read_env()
 
 CREDENTIALS_FILE = "data/creds.json"
+DATA_BASE_FILE = "data/db.db"
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMIN = env.str("ADMIN")
 spreadsheet_id = env.str("spreadsheet_id")
--- handlers/users/menu_handlers.py
+++ handlers/users/menu_handlers.py
@@ -5,24 +5,30 @@ from loader import dp
 from keyboards.default import menu
 from utils.google_sheets_data.calculations import calculate_cost
 from data.conf import GH_data
+from data.config import DATA_BASE_FILE
+from utils.db_api.sqlite import Database
+
+db = Database(DATA_BASE_FILE)
 
 
-@dp.message_handler(Command('menu'))
+@dp.message_handler(Command('start'))
 async def show_menu(message: Message):
+    if not db.user_exists(message.from_user.id):
+        db.add_user(message.from_user.id)
     await message.answer("Выбери действие", reply_markup=menu)
 
 
 @dp.message_handler(text="Посмотреть средний чек номера")
 async def show_average_cost(message: Message):
     params = await calculate_cost(GH_data['sofia'])
-    full_info = 'За {} дней средний чек: {:.2f},\n Недостача: {:.2f},'.format(
+    full_info = 'За {} дней средний чек: {:.2f},\nНедостача: {:.2f},'.format(
         params["days_gone"],
         params["sold_average"],
         params["shortage"],
     )
-    full_info += '\n Средний чек за номер на следующие дни: {:.2f}'.format(
+    full_info += '\nСредний чек за номер на следующие дни: {:.2f}'.format(
         params["next_days_cost"])
-    full_info += '\n Минимальна сумма прихода на следующие дни: {:.2f}'.format(
+    full_info += '\nМинимальна сумма прихода на следующие дни: {:.2f}'.format(
         params["next_days_cost"] * params["number_of_rooms"])
 
     await message.answer(text=full_info)
